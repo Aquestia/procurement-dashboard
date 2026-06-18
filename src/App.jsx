8fb73aec18da7e595ad0cb1bb18f77d5de8ec724
Shai1982 <shai.shamai10@gmail.com>
2026-06-18 13:26:35 +0300

    Remove: Summaries page

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import ProcurementView from './pages/ProcurementView'
 import TapiView from './pages/TapiView'
 import BackOrders from './pages/BackOrders'
 import Recommendations from './pages/Recommendations'
-import Summaries from './pages/Summaries'
 import FileManager from './pages/FileManager'
 import ImportNotes from './pages/ImportNotes'
 
@@ -103,7 +102,6 @@ export default function App() {
     tapi:            <TapiView data={data} notes={notes} saveNote={saveNote} loading={loading} />,
     backorders:      <BackOrders data={data} notes={notes} saveNote={saveNote} loading={loading} />,
     recommendations: <Recommendations data={data} notes={notes} loading={loading} />,
-    summaries:       <Summaries data={data} loading={loading} />,
     files:           <FileManager activeFile={activeFile} onFileChange={loadActiveFile} />,
     import:          <ImportNotes onDone={loadNotes} />,
   }
